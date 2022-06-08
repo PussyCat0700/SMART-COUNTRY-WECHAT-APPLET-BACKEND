0567d5fb4764c3e8f844afdce122ca71dab17113
刘一凡 <1729372667@qq.com>
2022-06-08 19:23:02 +0800

    图片返回URL

--- src/main/java/com/miniprogram/zhihuicunwu/controller/ImageTestController.java
+++ src/main/java/com/miniprogram/zhihuicunwu/controller/ImageTestController.java
@@ -1,5 +1,6 @@
 package com.miniprogram.zhihuicunwu.controller;
 
+import com.miniprogram.zhihuicunwu.util.Base64Util;
 import com.miniprogram.zhihuicunwu.util.ImageIOUtils;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,9 +12,14 @@ import java.io.IOException;
 @RestController
 @RequestMapping("imagetest")
 public class ImageTestController {
-    @PostMapping
-    public String queryById(@RequestBody String base64) throws IOException {
-        String relativePath = ImageIOUtils.uploadImg(base64);
+    @PostMapping("/base64")
+    public String base64a(@RequestBody String base64) throws IOException {
+        String relativePath = ImageIOUtils.uploadImg(Base64Util.replaceEnter(base64));
+        return ImageIOUtils.getUrlFromDBRecord(relativePath);
+    }
+    @PostMapping("/url")
+    public String url(@RequestBody String url) throws IOException {
+        String relativePath = ImageIOUtils.uploadImg(Base64Util.imageUrlToBase64(url));
         return ImageIOUtils.getUrlFromDBRecord(relativePath);
     }
 }
